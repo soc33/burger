4b888407d9bedcfa7f6bec3e3ce2c52150ed5f7b
soc33 <soc33@msn.com>
2019-03-06 20:36:08 -0500

    takes out sql info

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
     host: "localhost",
     port: "3306",
-    user: "root",
-    password: "password5",
+    user: "",
+    password: "",
     database: "burger_db"
 });
 
