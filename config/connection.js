6ab7d9dafa722fcb58fc333ca417faee15091274
soc33 <soc33@msn.com>
2019-03-06 21:31:07 -0500

    I was wrong

--- config/connection.js
+++ config/connection.js
@@ -6,12 +6,13 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-    host: "localhost",
-    port: "3306",
-    user: "",
-    password: "",
-    database: "burger_db"
-});
+        host: "localhost",
+        port: "3306",
+        user: "",
+        password: "",
+        database: "burger_db"
+    });
+}
 
 
 connection.connect(function (err) {
